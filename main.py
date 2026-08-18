2cea72d5cefa48060b98b31c2ec8a1793f341b60
user105109 <zouhairmessoudi00@gmail.com>
2026-08-18 16:59:43 +0100

    Deletion de field_validation de pydantic et faire la conversion et la validation manuellement avant le stockage de data et l'ajout d'un loggeur

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 from sqlmodel import SQLModel, Field, create_engine, Session
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from typing import Optional
-from pydantic import field_validator
 import uuid
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.FileHandler("app.log"), logging.StreamHandler()]
+)
+logger = logging.getLogger(__name__)
 
 class Position(SQLModel, table=True):
     id: Optional[str] = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
@@ -17,21 +22,30 @@ class Position(SQLModel, table=True):
     accuracy: Optional[float] = Field(default=None, ge=0)
     speed: Optional[float] = Field(default=None, ge=0)
 
-    @field_validator("timestamp", mode="before")
-    @classmethod
-    def parse_timestamp(cls, raw_date):
-        if isinstance(raw_date, str):
-            return datetime.fromisoformat(raw_date)
-        return raw_date
-
 DATABASE_URL = "sqlite:///gps.db"
 engine = create_engine(DATABASE_URL)
 SQLModel.metadata.create_all(engine)
 
 @app.post("/position")
 def create_position(position: Position):
-    with Session(engine) as session:
-        session.add(position)
-        session.commit()
-        session.refresh(position)
-    return position
+    try:
+        if isinstance(position.timestamp, str):
+            position.timestamp = datetime.fromisoformat( position.timestamp )
+        if position.timestamp.tzinfo is None:
+            position.timestamp = position.timestamp.replace( tzinfo=timezone.utc )
+
+        time_now = datetime.now(timezone.utc)
+        if position.timestamp > time_now + timedelta(minutes=5):
+            raise HTTPException(status_code=422, detail="Le temps est dans le future")
+        if position.timestamp < time_now - timedelta(days=1):
+            raise HTTPException(status_code=422, detail="Le temps est tres ancien (> 1j)")
+
+        with Session(engine) as session:
+            session.add(position)
+            session.commit()
+            session.refresh(position)
+        logger.info(f"Position enregistree: appareil: {position.device_id} id={position.id} latitude={position.latitude} longitude={position.longitude}")
+        return position
+    except Exception as e:
+        logger.error(f"Impossible d'enregistrer la position de l'appareil {position.device_id}: {e}")
+        raise
