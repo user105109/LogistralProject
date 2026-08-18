e65edd69110c5554cb43b2875b364fea1faef7c3
user105109 <zouhairmessoudi00@gmail.com>
2026-08-18 15:56:54 +0100

    Changer id d'un id numerote a un uuid auto generee pour chaque nouveau objet, puis la compression des deux classes en une seule en fixant le problem de stockage de datatime avec la validation et la conversion vers datatime en utilisant le field_validation de pydantic

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
-
 from sqlmodel import SQLModel, Field, create_engine, Session
 from datetime import datetime
 from typing import Optional
+from pydantic import field_validator
+import uuid
 
 class Position(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[str] = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
+    # default wud cause duplicating ids, but default_factory is perfect cuz it runs the func fresh each time a neew pos is created
     device_id: str
     latitude: float = Field(ge=-90, le=90)
     longitude: float = Field(ge=-180, le=180)
     timestamp: datetime
-    accuracy: Optional[float] = None
-    speed: Optional[float] = None
+    accuracy: Optional[float] = Field(default=None, ge=0)
+    speed: Optional[float] = Field(default=None, ge=0)
 
-class StorePosition(SQLModel):
-    device_id: str
-    latitude: float = Field(ge=-90, le=90)
-    longitude: float = Field(ge=-180, le=180)
-    timestamp: datetime
-    accuracy: Optional[float] = None
-    speed: Optional[float] = None
+    @field_validator("timestamp", mode="before")
+    @classmethod
+    def parse_timestamp(cls, raw_date):
+        if isinstance(raw_date, str):
+            return datetime.fromisoformat(raw_date)
+        return raw_date
 
 DATABASE_URL = "sqlite:///gps.db"
 engine = create_engine(DATABASE_URL)
 SQLModel.metadata.create_all(engine)
 
 @app.post("/position")
-def create_position(camion_data: StorePosition):
-    position = Position(**camion_data.model_dump())
+def create_position(position: Position):
     with Session(engine) as session:
         session.add(position)
         session.commit()
